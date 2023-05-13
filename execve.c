3bf5441fab1ef8f60c01c87d623d9de6503cf952
Williams <akanniwilliams@gmail.com>
2023-05-13 22:58:07 +0100

    Test out execve

---
+++ execve.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+	printf("Before execve\n");
+	if (execve(argv[0], argv, NULL) == -1)
+		perror("Error:");
+	printf("After execve\n");
+	return (0);
+}
