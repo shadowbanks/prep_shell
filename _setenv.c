25f5897a6e6c1b43d49b7dc38351f5914d74538d
Williams <akanniwilliams@gmail.com>
2023-05-20 22:55:20 +0100

    Handle unsetenv

--- _setenv.c
+++ _setenv.c
@@ -10,14 +10,19 @@ int _setenv(const char *name, const char *value, int overwrite)
 	//
 	my_env = malloc(sizeof(char *) * (size + 1));
 	if (!my_env)
-		return (-1);
+			return (-1);
 
 	//Make a copy of environ(env)
 	while(env[j])
 	{
 		my_env[j] = malloc(strlen(env[j]) + 1);
 		if (!my_env[j])
+		{
+			while (j)
+				free(my_env[--j]);
+			free(my_env);
 			return (-1);
+		}
 		strcpy(my_env[j], env[j]);
 		j++;
 	}
@@ -65,28 +70,22 @@ int _setenv(const char *name, const char *value, int overwrite)
 	//Set the new env if it does not exist
 	if (my_env[k] == NULL)
 	{
+		temp_env = realloc(my_env, (sizeof(char *) * (size + 2)));
+		if (!temp_env)
+		{
+			l = 0;
+			while (my_env[l])
+				free(my_env[l++]);
+			free(my_env);
+			free(new_env);
+			return (-1);
+		}
+		my_env = temp_env;
 		my_env[k++] = new_env;
 		my_env[k] = NULL;
 		//printf("Works fine\n");
 	}
 	//my_env[size - 1] = NULL;
-
-	temp_env = realloc(my_env, (sizeof(my_env) * (size + 2)));
-	if (!temp_env)
-	{
-		l = 0;
-		while (my_env[l])
-			free(my_env[l++]);
-		free(my_env);
-		free(new_env);
-		return (-1);
-	}
-	temp_env[k] = NULL;
-	my_env = temp_env;
-
-	while (env[j])
-		free(env[j++]);
-	free(env);
 
 	environ = my_env;
 
@@ -95,6 +94,43 @@ int _setenv(const char *name, const char *value, int overwrite)
 
 int _unsetenv(const char *name)
 {
+	char **my_env = NULL;
+	int k = 0, size = 0, i = 0;
+
+	while (environ[size++]);
+
+	//
+	my_env = malloc(sizeof(char *) * (size + 1));
+	if (!my_env)
+		return (-1);
+
+
+	//search for "name", if found skip while making a copy of environ
+	while (environ[k])
+	{
+		//printf("size: %d, k: %d\n", size, k);
+		if (strncmp(environ[k], name, strlen(name)) == 0 && environ[k][strlen(name)] == '=')
+		{
+			printf("%s SKIPPED k: %d\n", environ[k], k);
+			k++;
+			continue;
+		}
+		my_env[i] = malloc(strlen(environ[k]) + 1);
+		if (!my_env[i])
+		{
+			while(i)
+				free(my_env[--i]);
+			free(my_env);
+			return (-1);
+		}
+		//printf("%s\n", environ[k]);
+		strcpy(my_env[i], environ[k]);
+	//	printf("%s k: %d\n", my_env[k], k);
+		k++, i++;
+	}
+	//printf("size: %d, %s k-1: %d\n", size, my_env[k-1], k-1);
+	my_env[i] = NULL;
+	environ = my_env;
 	return (0);
 }
 
@@ -106,6 +142,11 @@ int main()
 	_setenv("VIP", "GhostProTech", 0);
 	_setenv("TEST", "1234", 1);
 	_setenv("VIP", "Ghost", 0);
+	_unsetenv("VIP");
+	_unsetenv("VIP2");
+	_setenv("VIP2", "Ghost", 0);
+	_unsetenv("VIP2");
+	_unsetenv("TEST");
 
 	while (environ[i])
 	{
